18688439fc402d3610034f02d318d6523bd177b2
Cokorda Raka Angga Jananuraga <raka.moviz@gmail.com>
2020-10-27 14:54:29 -0600

    Added JWT auth

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 2. pyenv virtualenv 3.7.3 crdjst-py
 3. pip install -r requirements.txt 
 4. export $(cat .env | xargs)
+5. curl -X POST -v --header "Content-Type: application/json"  http://127.0.0.1:5000/auth --data '{"username":"user1","password":"password"}'
+6. curl -v http://127.0.0.1:5000/rates?date=2020-10-20T12:00:00-05:00 --header "Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDM4MzIyNzYsImlhdCI6MTYwMzgzMTk3NiwibmJmIjoxNjAzODMxOTc2LCJpZGVudGl0eSI6MX0.oJco70C0Akj0VBwVe4ZzVxvZw0zv5OcuemeXN8aC3p8"
--- server.py
+++ server.py
@@ -1,13 +1,46 @@
+import os
 import json
 import flask
 from flask import request, jsonify, Response
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 from dateutil.parser import parse as parse_date, ParserError
+
+class User(object):
+  def __init__(self, id, username, password):
+    self.id = id
+    self.username = username
+    self.password = password
+
+  def __str__(self):
+    return "User(id='%s')" % self.id
+
+users = [
+  User(1, 'user1', 'password'),
+  User(2, 'user2', 'password'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+  user = username_table.get(username, None)
+  if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+      return user
+
+def identity(payload):
+  print(payload)
+  user_id = payload['identity']
+  return userid_table.get(user_id, None)
 
 def initialize (core_business, logger):
   app = flask.Flask(__name__)
-  app.config["DEBUG"] = True
+  app.config['DEBUG'] = True
+  app.config['SECRET_KEY'] = os.environ['ACCESS_TOKEN_SECRET']
+  jwt = JWT(app, authenticate, identity)
 
   @app.route('/rates', methods=['GET'])
+  @jwt_required()
   def obtain_rates():
     date_str = request.args.get('date')
     try:
